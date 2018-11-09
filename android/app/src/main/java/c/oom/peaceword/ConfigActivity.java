ccaf54919988590846c4c73c7bfd4651f6c4b795
oom- <venom0218@gmail.com>
2018-11-09 01:35:15 +0100

    Ajout frond Android

--- android/app/src/main/java/c/oom/peaceword/ConfigActivity.java
+++ android/app/src/main/java/c/oom/peaceword/ConfigActivity.java
@@ -1,13 +1,65 @@
 package c.oom.peaceword;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 public class ConfigActivity extends AppCompatActivity {
+
+    EditText dt2, dt3;
+    Switch sw2, sw3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        Typeface custom = ResourcesCompat.getFont(this, R.font.custom);
+
+        dt2 = findViewById(R.id.editText2);
+        dt3 = findViewById(R.id.editText3);
+        dt3.setTypeface(custom);
+        dt2.setTypeface(custom);
+        Button b1, b2, b3, b4;
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+        b3 = findViewById(R.id.button3);
+        b4 = findViewById(R.id.button4);
+        b1.setTypeface(custom);
+        b2.setTypeface(custom);
+        b3.setTypeface(custom);
+        b4.setTypeface(custom);
+        sw2 = findViewById(R.id.switch2);
+        sw3 = findViewById(R.id.switch3);
+        sw2.setTypeface(custom);
+        sw3.setTypeface(custom);
+    }
+
+    public void onExportSaltQRcode(View v)
+    {
+        Intent myIntent = new Intent(this, QRCodeActivity.class);
+        this.startActivity(myIntent);
+    }
+
+    public void onImport(View v)
+    {
+
+    }
+
+    public void onSaveConfig(View v)
+    {
+
+    }
+
+    public  void onCopySaltInClipboard(View v)
+    {
+
     }
 }
+
--- android/app/src/main/java/c/oom/peaceword/MainActivity.java
+++ android/app/src/main/java/c/oom/peaceword/MainActivity.java
@@ -1,8 +1,13 @@
 package c.oom.peaceword;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button but5 = findViewById(R.id.button5);
+        EditText dt = findViewById(R.id.editText);
+        Typeface custom = ResourcesCompat.getFont(this, R.font.custom);
+        but5.setTypeface(custom);
+        dt.setTypeface(custom);
+
     }
 
     public void openConfig(View v)
     {
-
+        Intent myIntent = new Intent(this, ConfigActivity.class);
+        this.startActivity(myIntent);
     }
 }
