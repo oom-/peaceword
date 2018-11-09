5ce73f9bd6d708728db4b9db2e3849c9fbffe993
oom- <11071671+oom-@users.noreply.github.com>
2018-11-09 01:35:15 +0100

    Ajout frond Android

---
+++ android/app/src/main/java/c/oom/peaceword/QRCodeActivity.java
@@ -0,0 +1,14 @@
+package c.oom.peaceword;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+
+    }
+}
